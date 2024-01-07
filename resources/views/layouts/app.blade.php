68c1b5a27910869d08e1b8557ae62bc69ffd60aa
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2024-01-08 07:19:58 +0800

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Sistem Jemaah Nazir | Welcome</title>
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>{{ config('app.name', 'Sistem Aplikasi Nazir') }}</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -13,6 +13,8 @@
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- Scripts -->
+  @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <!--
 `body` tag options:
