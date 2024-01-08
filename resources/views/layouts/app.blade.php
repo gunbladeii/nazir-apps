5a0662051cf83a65688c2f64f5c454d34ebdbc29
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2024-01-08 20:00:18 +0800

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -295,7 +295,7 @@
         </div><!-- /.col -->
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item"><a href="#">Halaman Utama</a></li>
             <li class="breadcrumb-item active">Pelantar Digital</li>
           </ol>
         </div><!-- /.col -->
